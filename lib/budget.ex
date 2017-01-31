2d528dd1dd80f30b074d4bb21ea5515854e0c383
Carlos Souza <carloshrsouza@gmail.com>
2017-01-31 10:00:33 -0500

    Use Stream and friendly print

--- lib/budget.ex
+++ lib/budget.ex
@@ -8,6 +8,7 @@ defmodule Budget do
         |> filter
         |> normalize
         |> sort
+        |> print
       {:error, _ } -> IO.puts "Error reading file"
     end
   end
@@ -16,18 +17,21 @@ defmodule Budget do
     CSV.parse_string(string)
   end
 
+  # ignores first row since we don't need it
   defp filter(rows) do
-    Enum.map(rows, fn(row) -> Enum.slice(row, 1, 3) end)
+    Stream.map(rows, fn(row) -> Enum.slice(row, 1, 3) end)
   end
 
   defp normalize(rows) do
-    Enum.map(rows, &(parse_amount(&1)))
+    Stream.map(rows, &(parse_amount(&1)))
   end
 
   defp parse_amount([date, description, amount]) do
     [date, description, parse_to_number(amount)]
   end
 
+  # converts amount in weird string
+  # format used by the bank to number
   defp parse_to_number(string) do
     Float.parse(string)
     |> elem(0)
@@ -40,5 +44,16 @@ defmodule Budget do
   
   defp order_asc_by_amount([_, _, prev], [_, _, next]) do
     prev < next
+  end
+
+  defp print(rows) do
+    IO.puts "\nTransactions:"
+    Stream.map(rows, &row_to_string/1)
+    |> Enum.to_list
+  end
+
+  defp row_to_string([date, description, amount]) do
+    # using :erlang.float_to_binary to convert 1.0e3 to 1000.00
+    IO.puts "#{date} #{description} $#{:erlang.float_to_binary(amount, decimals: 2)}"
   end
 end
