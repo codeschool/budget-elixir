ee54fd60e192f37d58872f736f3054c7a8202ee6
Carlos Souza <carloshrsouza@gmail.com>
2017-02-02 13:23:46 -0500

    Remove comments

--- lib/budget.ex
+++ lib/budget.ex
@@ -3,7 +3,6 @@ defmodule Budget do
   alias NimbleCSV.RFC4180, as: CSV
 
   def list_transactions do
-    # Use the version that raises if file does not exist
     File.read!("lib/transactions-jan.csv")
     |> parse
     |> filter
@@ -18,7 +17,6 @@ defmodule Budget do
     |> CSV.parse_string()
   end
 
-  # ignores first row since we don't need it
   defp filter(rows) do
     Enum.map(rows, &Enum.drop(&1, 1))
   end
@@ -31,12 +29,7 @@ defmodule Budget do
     [date, description, parse_to_number(amount)]
   end
 
-  # converts amount in weird string
-  # format used by the bank to number
-  # TODO: using floats here is not a good idea IMO.
-  # Or try to use integers or use the Decimal library.
   defp parse_to_number(string) do
-    # Using String.to_float since you want to raise in case of failures
     string
     |> String.to_float()
     |> abs
@@ -57,7 +50,6 @@ defmodule Budget do
   end
 
   defp row_to_string([date, description, amount]) do
-    # using :erlang.float_to_binary to convert 1.0e3 to 1000.00
     IO.puts "#{date} #{description} \t$#{:erlang.float_to_binary(amount, decimals: 2)}"
   end
 end
